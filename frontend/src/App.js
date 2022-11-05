0808f0683e2943169bac7c95357e871bae709fd1
SHUBHAM20CE070 <20ce070@charusat.edu.in>
2022-11-05 22:42:51 +0530

    new ultrachem app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>ULTRACHEM</a>
+      </header>
+      <main>
+        List of products
+      </main>
+    </div>
+  );
+}
+
+export default App;
